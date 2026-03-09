b05f79b746b0b3ad58a76463944b8da8d4e111ff
Heather Hudgeon <hhudgeon@gmail.com>
2026-03-08 19:58:51 -0500

    Add hh-books-block plugin scaffold

---
+++ wordpress/wp-content/plugins/hh-books-block/build/hh-books-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '452b8bc73f00d10f394f');
---
+++ wordpress/wp-content/plugins/hh-books-block/build/hh-books-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e5cd95f2227f9c0609c1');
